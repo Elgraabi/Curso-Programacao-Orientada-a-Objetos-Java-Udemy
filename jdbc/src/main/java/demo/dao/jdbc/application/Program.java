2ca95eb960100aa4c6d3b095d428e9235418805b
Elgraabi <silvatdb8@gmail.com>
2024-07-16 19:26:57 -0300

    feat:  class Seller

--- jdbc/src/main/java/demo/dao/jdbc/application/Program.java
+++ jdbc/src/main/java/demo/dao/jdbc/application/Program.java
@@ -1,14 +1,18 @@
 package demo.dao.jdbc.application;
 
+import java.util.Date;
+
 import demo.dao.jdbc.model.entities.Department;
+import demo.dao.jdbc.model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department dp = new Department(1, "Nooks");
-
+		Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.0, dp);
 		System.out.println(dp);
+		System.out.println(seller);
 	}
 
 }
