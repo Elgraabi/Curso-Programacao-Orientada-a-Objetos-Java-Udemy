7541ef2da80d687cee91a989e7239d4ab4ba30a3
Elgraabi <silvatdb8@gmail.com>
2024-07-17 15:59:55 -0300

    feat: Program, DaoFactory e SellerDaoJDBC

--- jdbc/src/main/java/demo/dao/jdbc/application/Program.java
+++ jdbc/src/main/java/demo/dao/jdbc/application/Program.java
@@ -1,22 +1,16 @@
 package demo.dao.jdbc.application;
 
-import java.util.Date;
-
 import demo.dao.jdbc.model.dao.DaoFactory;
 import demo.dao.jdbc.model.dao.SellerDao;
-import demo.dao.jdbc.model.entities.Department;
 import demo.dao.jdbc.model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dp = new Department(1, "Nooks");
-		Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.0, dp);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println(dp);
+		Seller seller = sellerDao.findById(3);
+
 		System.out.println(seller);
 	}
 
--- jdbc/src/main/java/demo/dao/jdbc/model/dao/DaoFactory.java
+++ jdbc/src/main/java/demo/dao/jdbc/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package demo.dao.jdbc.model.dao;
 
+import demo.dao.jdbc.db.DB;
 import demo.dao.jdbc.model.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao( ) {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 	
 }
