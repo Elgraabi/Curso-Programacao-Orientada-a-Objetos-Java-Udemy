f6a59b2f5b2bdff0c7514670fc45a2f6494d1d47
Elgraabi <silvatdb8@gmail.com>
2024-10-10 20:17:12 -0300

    feat: Inserção de usuário com POST

--- workshopmongo/workshopmongo/src/main/java/com/gabriel/workshopmongo/services/UserService.java
+++ workshopmongo/workshopmongo/src/main/java/com/gabriel/workshopmongo/services/UserService.java
@@ -7,23 +7,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.workshopmongo.domain.User;
+import com.gabriel.workshopmongo.dto.UserDTO;
 import com.gabriel.workshopmongo.repository.UserRepository;
 import com.gabriel.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 
 	}
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
 }
