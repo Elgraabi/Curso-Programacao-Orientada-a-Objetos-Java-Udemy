c8537a6925fedd743ef1393e6c893bf9cabc2a2f
Elgraabi <silvatdb8@gmail.com>
2024-10-11 21:43:20 -0300

    feat: Acrescentando comentários aos posts

--- workshopmongo/workshopmongo/src/main/java/com/gabriel/workshopmongo/domain/Post.java
+++ workshopmongo/workshopmongo/src/main/java/com/gabriel/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.gabriel.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.gabriel.workshopmongo.dto.AuthorDTO;
+import com.gabriel.workshopmongo.dto.CommentDTO;
 
 import jakarta.persistence.Id;
 
@@ -20,6 +23,7 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 		super();
@@ -77,6 +81,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
