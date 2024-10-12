7dd9c62e43061e4c162399c739604bb64f5bf9c5
Elgraabi <silvatdb8@gmail.com>
2024-10-11 21:24:34 -0300

    feat: Endpoint para retornar os posts de um usuário

--- workshopmongo/workshopmongo/src/main/java/com/gabriel/workshopmongo/resources/UserResources.java
+++ workshopmongo/workshopmongo/src/main/java/com/gabriel/workshopmongo/resources/UserResources.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.gabriel.workshopmongo.domain.Post;
 import com.gabriel.workshopmongo.domain.User;
 import com.gabriel.workshopmongo.dto.UserDTO;
 import com.gabriel.workshopmongo.services.UserService;
@@ -62,4 +63,10 @@ public class UserResources {
 		return ResponseEntity.noContent().build();
 	}
 
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
+	}
+	
 }
